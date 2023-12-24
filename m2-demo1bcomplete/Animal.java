1e9bd8360e0bd8280e0f8a5ef44c4de5ae1feb2a
jiwoon-yoo <124692189+jiwoon-yoo@users.noreply.github.com>
2023-12-24 16:51:59 +0900

    added

---
+++ m2-demo1bcomplete/Animal.java
@@ -0,0 +1,25 @@
+package demo1bcomplete;
+/**
+ * @Rich Smith at ZenOfProgramming.com
+ */
+public class Animal
+{
+   private AnimalType animalType;
+
+   public Animal (AnimalType type)
+   {
+      this.animalType = type;
+   }
+
+   public AnimalType getAnimalType ()
+   {
+      return animalType;
+   }
+
+   public void setAnimalType (AnimalType animalType)
+   {
+      this.animalType = animalType;
+   }
+
+
+}
---
+++ m2-demo1bcomplete/AnimalType.java
@@ -0,0 +1,10 @@
+package demo1bcomplete;
+
+/**
+ * @Rich Smith at ZenOfProgramming.com
+ */
+
+public enum AnimalType
+{
+   ELEPHANT, MONKEY, BEAR, LION, TIGER
+}
